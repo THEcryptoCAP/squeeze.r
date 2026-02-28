39a3b6762c1f6d84ca2329591dafa04567a5256d
THEcryptoCAP <cryptocapinoid@gmail.com>
2026-02-28 23:33:29 +0530

    bufreader

--- file_compressor/src/main.rs
+++ file_compressor/src/main.rs
@@ -1,3 +1,16 @@
+extern crate flate2;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::env::args;
+use std::fs::File;
+use std::io:BufReader;
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    if args().len() != 3{
+        eprintln("Usage: `source` `target`");
+        return;
+    }
+    let mut inputs = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
 }
